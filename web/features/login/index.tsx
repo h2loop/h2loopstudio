3fa816ee584077f1082813234a5037453d39f945
nova <novacode007@gmail.com>
2024-09-25 11:04:33 +0530

    refactor

--- web/features/login/index.tsx
+++ web/features/login/index.tsx
@@ -1,12 +1,14 @@
 import { Button, Card } from '@mantine/core'
 import { IconBrandGithub, IconBrandGoogle } from '@tabler/icons-react'
 import { signIn } from 'next-auth/react'
+import Image from 'next/image'
 import styles from './login.module.scss'
 
 export default function LoginScreen() {
   return (
     <div className={styles.loginScreen}>
       <Card className={styles.loginCard}>
+        <Image alt="logo" src="/images/logo.png" width={150} height={150} />
         <div className={styles.loginText}>Login to H2LooP.ai</div>
         <Button
           onClick={() => signIn('google')}
